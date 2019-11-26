aa95c7449200ea767af0a4d5196c213526f6a2de
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-11-26 15:29:44 +0200

    initial

---
+++ data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+df_train_x = pd.read_csv('football_train_x.csv')
+df_train_y = pd.read_csv('football_train_y.csv')
+print(df_train_x)
