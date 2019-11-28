f9dabadc91709609a340374db881287f80815bcf
Christian Segercrantz <segercc1@aalto.fi>
2019-11-28 16:51:40 +0200

    Started linear regression and logistc regression

--- data.py
+++ data.py
@@ -9,6 +9,10 @@ from sklearn.model_selection import train_test_split
 import seaborn as sns
 from imblearn.over_sampling import SMOTE
 import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+
 
 df_train_x = pd.read_csv('football_train_x.csv')
 df_train_y = pd.read_csv('football_train_y.csv')
@@ -19,9 +23,25 @@ df_train_complete = pd.concat([df_train_x,df_train_y],axis = 1)
 print(df_train_complete.groupby("Interest").mean())
 plt.matshow(df_train_complete)
 
+#---------------------Linear Regression------------
+for name in df_train_x.columns:
+    X = df_train_x[name]
+    y = df_train_y['FTG']
 
+    # Note the difference in argument order
+    model = sm.OLS(y, X).fit()
+    predictions = model.predict(X) # make the predictions by the model
 
-#---------------------
+    # Save R^2 in vaiable
+    r_squared = {}
+    r_squared[name] = model.rsquared
+
+    # Print out the statistics
+    #print(model.summary())
+#--------------------------------------------------
+print(r_squared)
+
+#---------------------Logarithmic Regression------------
 os = SMOTE(random_state=0)
 columns =df_train_x.columns
 os_data_X,os_data_y=os.fit_sample(df_train_x, df_train_y["Interest"])
@@ -29,21 +49,28 @@ os_data_X = pd.DataFrame(data=os_data_X,columns=columns )
 os_data_y= pd.DataFrame(data=os_data_y,columns=['y'])
 # we can Check the numbers of our data
 print("length of oversampled data is ",len(os_data_X))
-print("Number of no subscription in oversampled data",len(os_data_y[os_data_y['y']==0]))
-print("Number of subscription",len(os_data_y[os_data_y['y']==1]))
+print("Number of none interesting matches in oversampled data",len(os_data_y[os_data_y['y']==0]))
+print("Number of interesting matches",len(os_data_y[os_data_y['y']==1]))
 print("Proportion of no subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==0])/len(os_data_X))
 print("Proportion of subscription data in oversampled data is ",len(os_data_y[os_data_y['y']==1])/len(os_data_X))
-#---------------------
-
 
 logreg = LogisticRegression()
 
-rfe = RFE(logreg, 20)
+rfe = RFE(logreg, 5)
 rfe = rfe.fit(os_data_X, os_data_y.values.ravel())
-print(rfe.support_)
-print(rfe.ranking_)
+#print(rfe.support_)
+#print(rfe.ranking_)
+#print((df_train_x.T[rfe.support_]).T)
+df_train_x_rfe = (df_train_x.T[rfe.support_]).T
+df_test_x_rfe = (df_test_x.T[rfe.support_]).T.drop(columns=['HTR'])
 
-
-logit_model=sm.Logit(df_train_y,df_train_x)
+logit_model=sm.Logit(df_train_y["Interest"],df_train_x_rfe)
 result=logit_model.fit()
 print(result.summary2())
+
+logreg.fit(df_train_x_rfe, df_train_y["Interest"])
+y_pred = logreg.predict(df_test_x_rfe)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(df_test_x_rfe, df_test_y["Interest"])))
+confusion_matrix = confusion_matrix(df_test_y["Interest"], y_pred)
+print(confusion_matrix)
+#------------------------------------------------------
