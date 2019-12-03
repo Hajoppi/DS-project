3c97503337eea7ac5434fdc0827c89173475ab7f
Tuomas Kontola <tuomas.j.kontola@aalto.fi>
2019-12-03 15:28:54 +0200

    Prediction values

--- data.py
+++ data.py
@@ -30,11 +30,81 @@ df_train_complete = pd.concat([df_train_x,df_train_y],axis = 1)
 print(df_train_complete.groupby("Interest").mean())
 #plt.matshow(df_train_complete)
 
-#---------------------Linear Regression------------
-scaled_df = StandardScaler().fit_transform(df_train_x)
-pca = PCA(n_components=1)
+#--------------------- RFE features for FTG------------
+target_value="FTG"
+y = df_train_y[target_value]
+X = df_train_x
+#no of features
+nof_list=np.arange(1,13)            
+high_score=0
+#Variable to store the optimum features
+nof=0           
+score_list =[]
+for n in range(len(nof_list)):
+    X_train, X_test, y_train, y_test = train_test_split(df_train_x,y, test_size = 0.3, random_state = 0)
+    model = LinearRegression()
+    rfe = RFE(model,nof_list[n])
+    X_train_rfe = rfe.fit_transform(X_train,y_train)
+    X_test_rfe = rfe.transform(X_test)
+    model.fit(X_train_rfe,y_train)
+    score = model.score(X_test_rfe,y_test)
+    score_list.append(score)
+    if(score>high_score):
+        high_score = score
+        nof = nof_list[n]
+print("Optimum number of features: %d" %nof)
+print("Score with %d features: %f" % (nof, high_score))
+
+cols = list(df_train_x.columns)
+model = LinearRegression()
+#Initializing RFE model
+rfe = RFE(model, nof)             
+#Transforming data using RFE
+X_rfe = rfe.fit_transform(X,y)  
+#Fitting the data to model
+model.fit(X_rfe,y)              
+temp = pd.Series(rfe.support_,index = cols)
+selected_features_rfe = temp[temp==True].index
+print("RFE SELECTED FEATURES", selected_features_rfe)
+
+#---------------------Backward Elimination features for FTG------------
+X_1 = sm.add_constant(df_train_x)
+y = df_train_y[target_value]
+model = sm.OLS(y,X_1).fit()
+model.pvalues
+
+cols = list(df_train_x.columns)
+pmax = 1
+while (len(cols)>0):
+    p= []
+    X_1 = df_train_x[cols]
+    X_1 = sm.add_constant(X_1)
+    model = sm.OLS(y,X_1).fit()
+    p = pd.Series(model.pvalues.values[1:],index = cols)      
+    pmax = max(p)
+    feature_with_p_max = p.idxmax()
+    if(pmax>0.05):
+        cols.remove(feature_with_p_max)
+    else:
+        break
+
+selected_features_BE = cols
+print("Selected features with backward propagations")
+print(selected_features_BE)
+
+#---------------------PCA Components------------
+scaled_df = StandardScaler().fit_transform(df_train_x[selected_features_rfe])
+pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(scaled_df)
-pca_df = pd.DataFrame(data=principalComponents, columns=['PCA1'])
+print("Explained variance",pca.explained_variance_)
+print("Explained variance ratio",pca.explained_variance_ratio_)
+print("Explained components",pca.n_features_)
+print("Explained components _",pca.n_components_)
+pca_df = pd.DataFrame(data=principalComponents, columns=['PCA1', 'PCA2'])
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.scatter(pca_df["PCA1"], pca_df["PCA2"])
+plt.show()
 pca_data = pca_df["PCA1"].values.reshape(-1,1)
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(pca_data, df_train_y["FTG"])  # perform linear regression
@@ -43,8 +113,25 @@ plt.scatter(pca_data, df_train_y["FTG"])
 plt.plot(pca_data, Y_pred, color='red')
 plt.show()
 
+#---------------------Linear Regression 0 ------------
+print()
+print("Linear regression")
+X = df_train_x[selected_features_BE]
+x_test = df_test_x[selected_features_BE]
+y = df_train_y[["FTG"]]
+reg = LinearRegression().fit(X,y)
+print("Score", reg.score(X, y))
+print("Coef", reg.coef_)
+y_pred = reg.predict(x_test)
+plt.plot(y_pred-df_test_y[["FTG"]].values)
+#plt.plot(x_test["HomeTeam"], y_pred, color="red")
+#plt.xticks(())
+#plt.yticks(())
+plt.show()
+
+#---------------------Linear Regression------------
 r_squared = {}
-for name in df_train_x.columns:
+for name in selected_features_rfe: #df_train_x.columns:
     X = df_train_x[name]
     y = df_train_y['FTG']
 
@@ -125,79 +212,22 @@ print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(lo
 confusion_matrix = confusion_matrix(df_test_y["Interest"], y_pred)
 print(confusion_matrix)
 #------------------------------------------------------
-
+target_value = "Interest"
 #---------------------Correlation matrix features for FTG------------
 cor = df_train_complete.corr()
 np.fill_diagonal(cor.values, np.nan)
-cor_target = abs(cor["FTG"])
-relevant_features = cor_target[cor_target>0.3]
+plt.matshow(cor)
+#plt.show()
+cor_target = abs(cor[target_value])
+relevant_features = cor_target[cor_target>0.25]
 print("Relevant features by correlation matrix")
 print(relevant_features)
 
-#---------------------Backward Elimination features for FTG------------
-X_1 = sm.add_constant(df_train_x)
-y = df_train_y["FTG"]
-model = sm.OLS(y,X_1).fit()
-model.pvalues
 
-cols = list(df_train_x.columns)
-pmax = 1
-while (len(cols)>0):
-    p= []
-    X_1 = df_train_x[cols]
-    X_1 = sm.add_constant(X_1)
-    model = sm.OLS(y,X_1).fit()
-    p = pd.Series(model.pvalues.values[1:],index = cols)      
-    pmax = max(p)
-    feature_with_p_max = p.idxmax()
-    if(pmax>0.05):
-        cols.remove(feature_with_p_max)
-    else:
-        break
-
-selected_features_BE = cols
-print("Selected features with backward propagations")
-print(selected_features_BE)
-
-#--------------------- RFE features for FTG------------
-y = df_train_y["FTG"]
-X = df_train_x
-#no of features
-nof_list=np.arange(1,13)            
-high_score=0
-#Variable to store the optimum features
-nof=0           
-score_list =[]
-for n in range(len(nof_list)):
-    X_train, X_test, y_train, y_test = train_test_split(df_train_x,y, test_size = 0.3, random_state = 0)
-    model = LinearRegression()
-    rfe = RFE(model,nof_list[n])
-    X_train_rfe = rfe.fit_transform(X_train,y_train)
-    X_test_rfe = rfe.transform(X_test)
-    model.fit(X_train_rfe,y_train)
-    score = model.score(X_test_rfe,y_test)
-    score_list.append(score)
-    if(score>high_score):
-        high_score = score
-        nof = nof_list[n]
-print("Optimum number of features: %d" %nof)
-print("Score with %d features: %f" % (nof, high_score))
-
-cols = list(df_train_x.columns)
-model = LinearRegression()
-#Initializing RFE model
-rfe = RFE(model, nof)             
-#Transforming data using RFE
-X_rfe = rfe.fit_transform(X,y)  
-#Fitting the data to model
-model.fit(X_rfe,y)              
-temp = pd.Series(rfe.support_,index = cols)
-selected_features_rfe = temp[temp==True].index
-print(selected_features_rfe)
 
 #--------------------- Embeded method features for FTG------------
 
-y = df_train_y["FTG"]
+y = df_train_y[target_value]
 X = df_train_x
 reg = LassoCV()
 reg.fit(X, y)
@@ -212,3 +242,4 @@ import matplotlib
 matplotlib.rcParams['figure.figsize'] = (8.0, 10.0)
 imp_coef.plot(kind = "barh")
 plt.title("Feature importance using Lasso Model")
+#plt.show()
